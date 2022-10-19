73624b281c150dd015d52c454ce0b6c801851714
MarioMH8 <mmenendez@magnus-strategy.com>
2022-10-19 20:56:18 +0200

    fix: lintear proyecto

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -9,4 +9,7 @@ module.exports = {
 			},
 		},
 	],
+	rules: {
+		'@typescript-eslint/promise-function-async': 'off'
+	}
 };
--- src/promise.ts
+++ src/promise.ts
@@ -12,23 +12,27 @@ type EventNames = 'stderr' | 'stdout';
 type Resolve<T> = (value: Promise<T> | T) => void;
 type Reject = (reason?: any) => void;
 type Executor<T> = (resolve: Resolve<T>, reject: Reject, event: EventEmitter) => void;
+type Listener = (...args: any[]) => void;
 
 export default class ChildProcessPromise extends Promise<SpawnResult> {
 	private readonly ee: EventEmitter;
 
 	constructor(executor: Executor<SpawnResult>) {
-		let rej: Reject | undefined = undefined;
-		let res: Resolve<SpawnResult> | undefined = undefined;
+		let rej: Reject | undefined;
+		let res: Resolve<SpawnResult> | undefined;
 		super((resolve, reject) => {
 			rej = reject;
 			res = resolve;
 		});
 		this.ee = new EventEmitter();
-		if (res && rej) executor(res, rej, this.ee);
+		if (res && rej) {
+			executor(res, rej, this.ee);
+		}
 	}
 
-	addListener(event: EventNames, listener: (...args: any[]) => void): ChildProcessPromise {
+	addListener(event: EventNames, listener: Listener): ChildProcessPromise {
 		this.ee.addListener(event, listener);
+
 		return this;
 	}
 
@@ -44,51 +48,59 @@ export default class ChildProcessPromise extends Promise<SpawnResult> {
 		return this.ee.listenerCount(event);
 	}
 
-	listeners(event: EventNames): Function[] {
-		return this.ee.listeners(event);
+	listeners(event: EventNames): Listener[] {
+		return this.ee.listeners(event) as Listener[];
 	}
 
-	off(event: EventNames, listener: (...args: any[]) => void): ChildProcessPromise {
+	off(event: EventNames, listener: Listener): ChildProcessPromise {
 		this.ee.off(event, listener);
+
 		return this;
 	}
 
-	on(event: EventNames, listener: (...args: any[]) => void): ChildProcessPromise {
+	on(event: EventNames, listener: Listener): ChildProcessPromise {
 		this.ee.on(event, listener);
+
 		return this;
 	}
 
-	once(event: EventNames, listener: (...args: any[]) => void): ChildProcessPromise {
+	once(event: EventNames, listener: Listener): ChildProcessPromise {
 		this.ee.once(event, listener);
+
 		return this;
 	}
 
-	prependListener(event: EventNames, listener: (...args: any[]) => void): ChildProcessPromise {
+	prependListener(event: EventNames, listener: Listener): ChildProcessPromise {
 		this.ee.prependListener(event, listener);
+
 		return this;
 	}
 
-	prependOnceListener(event: EventNames, listener: (...args: any[]) => void): ChildProcessPromise {
+	prependOnceListener(event: EventNames, listener: Listener): ChildProcessPromise {
 		this.ee.prependOnceListener(event, listener);
+
 		return this;
 	}
 
-	rawListeners(event: EventNames): Function[] {
-		return this.ee.rawListeners(event);
+	rawListeners(event: EventNames): Listener[] {
+		return this.ee.rawListeners(event) as Listener[];
 	}
 
 	removeAllListeners(event: EventNames): ChildProcessPromise {
 		this.ee.removeAllListeners(event);
+
 		return this;
 	}
 
-	removeListener(event: EventNames, listener: (...args: any[]) => void): ChildProcessPromise {
+	removeListener(event: EventNames, listener: Listener): ChildProcessPromise {
 		this.ee.removeListener(event, listener);
+
 		return this;
 	}
 
 	setMaxListeners(max: number): ChildProcessPromise {
 		this.ee.setMaxListeners(max);
+
 		return this;
 	}
 }
